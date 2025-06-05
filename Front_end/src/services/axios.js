8564e2e18f408ef4e955c363f617b0460536f741
anthony-garcia-santos <Anthony.garcia.santos>
2025-06-05 04:22:30 -0300

    atualização para deploy Frontend

--- Front_end/src/services/axios.js
+++ Front_end/src/services/axios.js
@@ -3,14 +3,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 5000,
-  withCredentials: true 
-  
+  withCredentials: true,
 });
 
 export default api;
-
-
 
 
