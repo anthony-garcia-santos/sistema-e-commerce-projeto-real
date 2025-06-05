3fc9066523719000a63729a445297e51aa478a73
anthony-garcia-santos <Anthony.garcia.santos>
2025-06-05 08:49:02 -0300

    backup

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,13 +1,10 @@
-//Front_end/src/services/axios.js
-
+// Front_end/src/services/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL.replace(/\/api$/, ''), // Remove /api do final
   timeout: 5000,
   withCredentials: true,
 });
 
 export default api;
-
-
